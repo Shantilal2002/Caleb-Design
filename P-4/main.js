aed0410cb9a7613c56385588f87f7b7a64e1640d
Shantilal Parmar <shantilalparmar2002@gmail.com>
2025-01-22 12:52:10 +0530

    Add files via upload

---
+++ P-4/main.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:2000,
+    
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        },
+        1400:{
+            items:5
+        }
+    }
+})
